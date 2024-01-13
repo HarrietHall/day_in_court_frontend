ff3cf322e1b7e03dbf2c35263d3845c2af92708b
Harriet <harriethall@github.com>
2024-01-13 16:37:28 +0000

    create local app data

---
+++ data/data.jsx
@@ -0,0 +1,51 @@
+export const courtTypeOptions = [
+    { label: "Domestic Violence", id: 1 },
+    { label: "Trials", id: 2 },
+    { label: "Youth", id: 3 },
+    { label: "Custody", id: 4},
+    { label: "Other", id: 5 },
+];
+
+export const magistrateOptions = [
+    { label: "Thomas Smith", id: "Thomas Smith" },
+    { label: "Patty Grace", id: "Patty Grace" },
+    { label: "Douglas Weather", id: "Douglas Weather" },
+    { label: "Mick Stack", id: "Mick Stack" },
+    { label: "Peter James", id: "Peter James" },
+    { label: "Herbert Heckle", id: "Herbert Heckle" },
+    { label: "Harrison Grass", id: "Harrison Grass" },
+    { label: "Lucie Ray", id: "Lucie Ray" },
+];
+
+export const clerkOptions = [
+    { label: "Sarah Steps", id: "Sarah Steps" },
+    { label: "Paul Potter", id: "Paul Potter" },
+    { label: "Maggie Dusk", id: "Maggie Dusk" },
+    { label: "Ledger Kay", id: "Ledger Kay" },
+    { label: "Penny Blake", id: "Penny Blake" },
+    { label: "Steven Strange", id: "Steven Strange" },
+    { label: "Jack Bricks", id: "Jack Bricks" },
+    { label: "Jack Jackson", id: "Jack Jackson" },
+];
+
+export const usherOptions = [
+    { label: "Thomas Smith", id: "Thomas Smith" },
+    { label: "Patty Grace", id: "Patty Grace" },
+    { label: "Douglas Weather", id: "Douglas Weather" },
+    { label: "Mick Stack", id: "Mick Stack" },
+    { label: "Peter James", id: "Peter James" },
+    { label: "Herbert Heckle", id: "Herbert Heckle" },
+    { label: "Harrison Grass", id: "Harrison Grass" },
+    { label: "Lucie Ray", id: "Lucie Ray" },
+];
+
+export const casesOptions = [
+    { label: "123345", id: "123345" },
+    { label: "323345", id: "323345" },
+    { label: "123347", id: "123347" },
+    { label: "123444", id: "123444" },
+    { label: "126324", id: "126324" },
+    { label: "126300", id: "126300" },
+    { label: "126333", id: "126333" },
+    { label: "125522", id: "125522" },
+];
