02fec03863c1741f8b192e3f86fc53d889a1acbc
Harriet <harriethall@github.com>
2024-01-13 12:51:38 +0000

    incorporate case completion screen and bottom nav

--- day-in-court-project/day_in_court_frontend/App.jsx
+++ day-in-court-project/day_in_court_frontend/App.jsx
@@ -1,14 +1,13 @@
-
 import React from "react";
+import BottomTabs from "./navigation_bar/bottom_tab";
 import { StyleSheet } from "react-native";
-
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import HomeScreen from "./screens/home";
 import { PaperProvider } from "react-native-paper";
 import CourtFormScreen from "./screens/court_form_screen";
 import CourtScreen from "./screens/court_completed_screen";
-
+import CaseCompletedScreen from "./screens/case_completed_screen";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
 
@@ -16,7 +15,6 @@ const Stack = createStackNavigator();
 
 export default function App() {
   return (
- 
     <SafeAreaProvider>
       <GestureHandlerRootView style={styles.container}>
         <SafeAreaView style={styles.container}>
@@ -35,13 +33,17 @@ export default function App() {
                   component={CourtFormScreen}
                 />
                 <Stack.Screen name="court_completed" component={CourtScreen} />
+                <Stack.Screen
+                  name="case_completed"
+                  component={CaseCompletedScreen}
+                />
               </Stack.Navigator>
+              <BottomTabs />
             </NavigationContainer>
           </PaperProvider>
         </SafeAreaView>
       </GestureHandlerRootView>
     </SafeAreaProvider>
- 
   );
 }
 
